ad7f3bf5180bfd894d0860b2b9e19e13f40ff9e0
PizaTheCat <wuglenn.wg@gmail.com>
2017-12-18 20:26:44 +0800

    .
    made a program to record the weather details every 30 minutes

--- timer.py
+++ timer.py
@@ -1,11 +1,19 @@
 from datetime import datetime
-from threading import Timer
+# from threading import Timer
 from OWMfunction import weatherforcast
+import time as t
 # day = x.day + 1, hour = 1, minute = 0, second + 10
-x = datetime.today()
-y = x.replace(x.minute + 30, second = 0)
-delta_t = y - x
-secs = delta_t.seconds + 1
-
-t = Timer(secs, weatherforcast('Singapore'))
-t.start()
+# x = datetime.today()
+# y = x.replace(second = x.second + 5)
+# delta_t = y - x
+# secs = delta_t.seconds + 1
+#
+# t = Timer(secs, weatherforcast('Singapore'))
+# # t.start()
+# from threading import Timer
+# weatherforcast('Singapore')
+# t = Timer(60.0, weatherforcast('Singapore'))
+# t.start
+while True:
+    weatherforcast('Singapore')
+    t.sleep(108000)
